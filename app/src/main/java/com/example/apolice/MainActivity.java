2739dca0d8f751ac1b2f82f17ae198477e7f0c67
Henrique Oliveira <64371355+Henrique-Oli-Mattos@users.noreply.github.com>
2022-03-21 20:00:17 -0300

    Add files via upload
    Erro corrigido

--- app/src/main/java/com/example/apolice/MainActivity.java
+++ app/src/main/java/com/example/apolice/MainActivity.java
@@ -1,15 +1,12 @@
-package com.example.apolice;
+package com.example.apolicemobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
 
-
-
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     EditText textName, textNumber, textIdade, textSexo, textValor;
     TextView textApolice;
@@ -27,29 +24,25 @@ public class MainActivity extends AppCompatActivity  {
         textValor = findViewById(R.id.textValor);
 
         textApolice = findViewById(R.id.textApolice);
+    }
+
+    //Metodo calcular:
+
+    //Erro na hora de fechar chave (Corrigido)
+    public void calcular(View view){
 
 
-        //Erro na criação do metodo!
-
-        /*
-        public void calcular(View view){
-
-        }
-         */
+        int number = Integer.parseInt(textNumber.getText().toString());
+        String name = textName.getText().toString();
+        int idade = Integer.parseInt(textIdade.getText().toString());
+        char sexo = textSexo.getText().toString().charAt(0);    //charAt para converter em Char
+        double valor = Double.parseDouble(textValor.getText().toString());
 
 
-        //Metodo calcular:
-            int number = Integer.parseInt(textNumber.getText().toString());
-            String name = textName.getText().toString();
-            int idade = Integer.parseInt(textIdade.getText().toString());
-            char sexo = textSexo.getText().toString().charAt(0);    //charAt para converter em Char
-            double valor = Double.parseDouble(textValor.getText().toString());
+        Apolice apolice = new Apolice(number, name, idade, sexo, valor);
+        //Verificar com professor se é possivel usar Spring e injeção de dependências
 
-
-            Apolice apolice = new Apolice(number, name, idade, sexo, valor);
-            //Verificar com professor se é possivel usar Spring e injeção de dependências
-
-            textApolice.setText("R$ " + apolice.calcularValor());
+        textApolice.setText("R$ " + apolice.calcularValor());
         //Acaba metodo
 
     }
